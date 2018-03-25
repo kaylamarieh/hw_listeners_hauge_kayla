f87e85545e1de258d90743e96f31b28569828c25
Kayla Hauge <kmhauge@ufl.edu>
2018-03-25 11:52:07 -0400

    add event listener when submit is pressed

--- js/main.js
+++ js/main.js
@@ -2,4 +2,7 @@ var display = document.getElementbyId('newsletter');
 
 display.addEventListener('submit', function (event) {
     event.preventDefault();
+    //var myElement = document.createElement('p');
+    //myElement.innerHTML = 'Thank you for signing up!';
+    //document.body.appendChild(myElement);
 });
