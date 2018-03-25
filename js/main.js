b9d61f42fe212f4a20644a85e59f69c4e9c005ab
Kayla Hauge <kmhauge@ufl.edu>
2018-03-25 12:28:33 -0400

    add comments to submit event listener

--- js/main.js
+++ js/main.js
@@ -1,8 +1,10 @@
 var myForm = document.getElementById('newsletter');
 
+//listens for the submit on the form
 myForm.addEventListener('submit', function (event) {
     event.preventDefault();
     var myElement = document.createElement('p');
+    //adds a thank you message to the page after submission
     myElement.innerHTML = 'Thank you for signing up!';
     document.getElementById('newsletter').appendChild(myElement);
 });
