0fbf5843097c09e02ebed8e961a691b94c5e8ad0
Kayla Hauge <kmhauge@ufl.edu>
2018-03-25 19:57:52 -0400

    add event listener for scroll

--- js/main.js
+++ js/main.js
@@ -8,3 +8,29 @@ myForm.addEventListener('submit', function (event) {
     myElement.innerHTML = 'Thank you for signing up!';
     document.getElementById('newsletter').appendChild(myElement);
 });
+
+//var show = document.getElementById('appear');
+
+//show.addEventListener('scroll', function () {
+    //var animation =
+    //document.getElementsById('appear').animate([
+        //{ transform: 'translateY(0)' },
+        //{ transform: 'translateY(-100%)' }
+    //]);
+//});
+
+//var show = document.getElementById('work');
+
+var show = window
+
+//function opcChange() {
+    //var amount = 'opacity = 1';
+    //document.getElementsByClassName('appear').style.opacity = amount;
+//}
+
+//show.addEventListener('scroll', opcChange);
+
+
+show.addEventListener('scroll', function () {
+    document.getElementById('work').style.opacity = '1';
+});
