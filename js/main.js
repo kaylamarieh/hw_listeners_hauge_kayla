2b8d63e78ed6cc1fa14d91572599ec82dac6a617
Kayla Hauge <kmhauge@ufl.edu>
2018-03-25 21:17:48 -0400

    add event listeners for scroll

--- js/main.js
+++ js/main.js
@@ -9,28 +9,34 @@ myForm.addEventListener('submit', function (event) {
     document.getElementById('newsletter').appendChild(myElement);
 });
 
-//var show = document.getElementById('appear');
-
-//show.addEventListener('scroll', function () {
-    //var animation =
-    //document.getElementsById('appear').animate([
-        //{ transform: 'translateY(0)' },
-        //{ transform: 'translateY(-100%)' }
-    //]);
-//});
-
-//var show = document.getElementById('work');
-
-var show = window
-
-//function opcChange() {
-    //var amount = 'opacity = 1';
-    //document.getElementsByClassName('appear').style.opacity = amount;
-//}
-
-//show.addEventListener('scroll', opcChange);
-
+//listens for scroll on window
+var show = window;
 
 show.addEventListener('scroll', function () {
-    document.getElementById('work').style.opacity = '1';
+    //changes the opacity on icons to show up
+    document.getElementById('one').style.opacity = '1';
+    document.getElementById('two').style.opacity = '1';
+    document.getElementById('three').style.opacity = '1';
+});
+
+
+
+
+
+var info = document.getElementById('begin');
+
+//listens for a click on the word 'Begginer'
+info.addEventListener('click', function () {
+    var more = document.createElement('p');
+    //adds more inoformation about the beginner option under the words
+    more.innerHTML = 'If you have just started out, but want to learn more.';
+    document.getElementById('moreinfo').appendChild(more);
+});
+
+var other = document.getElementById('inter');
+
+other.addEventListener('click', function () {
+    var most = document.createElement('p');
+    most.innerHTML = 'If you know some, but would like to expand your knowledge';
+    document.getElementById('moreinfo').removeChild(('moreinfo').firstChild);
 });
